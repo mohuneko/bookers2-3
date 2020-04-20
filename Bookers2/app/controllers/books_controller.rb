13aaebdd71430f5078899edab5d69df7758ee9f2
mohuneko <b0wdmohu@gmail.com>
2020-04-20 22:41:19 +0900

    booksページ途中

--- Bookers2/app/controllers/books_controller.rb
+++ Bookers2/app/controllers/books_controller.rb
@@ -5,24 +5,31 @@ class BooksController < ApplicationController
   end
 
   def index
+
+   @book = Book.new #投稿フォーム
    @books = Book.all #記事を全件取得
   end
 
   def new
-   @book = Book.new #投稿フォーム
   end
 
   def create
-   book = Book.new(book_params) #表示された投稿を保存
-   book.save
-   redirect_to books_path
+    @book = Book.new(book_params) #表示された投稿を保存
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def edit
+   @book = Book.find(params[:id]) #既存のレコードを取得している。updateで動く
   end
 
-private
- def book_params
-  params.require(:book).permit(:title, :opinion)
-end
+  private
+  def book_params
+   params.require(:book).permit(:title, :opinion, :user_id)
+  end
+
+ #def  create_params
+  #params.require(:book).permit(:title, :opinion
+#end
 end
